d92510b33e7519fac55dcd063b0d1a1d27350a7f
Viktor Österholm <Ozterkvlt@hotmail.com>
2021-01-03 18:49:04 +0200

    receive and unzip file

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
-const http = require("http")
-const port = 3001
+const express = require('express');
+const formidable = require('formidable');
+var fs = require("fs");
+var JSZip = require("JSZip")
+const path = "./tests/"
 
-const app = http.createServer(function (req, res) {
-    res.write("Hello Node")
-    res.end()
-})
-// POST method route
-app.post('/test', function (req, res) {
-    res.send('test has been stored')
+const app = express();
+
+//start app 
+const port = process.env.PORT || 3001;
+
+app.post('/tests', (req, res, next) => {
+    const form = formidable();
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        console.log(files.key);
+        //var zip = new AdmZip(files.key);
+        // extracts the specified file to the specified location
+        fs.readFile(files.key.path, function (err, data) {
+            if (!err) {
+                var zip = new JSZip();
+                zip.loadAsync(data).then(function (contents) {
+                    Object.keys(contents.files).forEach(function (filename) {
+                        zip.file(filename).async('nodebuffer').then(function (content) {
+                            var dest = path + filename;
+                            fs.writeFileSync(dest, content);
+                        });
+                    });
+                });
+            }
+        });
+        //zip.extractAllTo("/tests/", true);
+        res.json({ fields, files });
+    });
+});
+
+//create tests directory if it doesnt exist
+const dir = "tests";
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello node")
 })
 
 app.listen(port, function (error) {
